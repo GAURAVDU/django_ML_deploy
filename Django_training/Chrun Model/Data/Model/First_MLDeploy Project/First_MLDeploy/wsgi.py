e6a685de942fbe6a2e9b1e52b0538b4ea1d90b3d
GAURAVDU <imgauravdubey@gmail.com>
2022-01-20 20:57:35 +0530

    Done Sucessfully!!

--- Django_training/Chrun Model/Data/Model/First_MLDeploy Project/First_MLDeploy/wsgi.py
+++ Django_training/Chrun Model/Data/Model/First_MLDeploy Project/First_MLDeploy/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'First_MLDeploy.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "First_MLDeploy.settings")
 
 application = get_wsgi_application()
